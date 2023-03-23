d2443fa08c2b63aa3842e805e63ed81d16247b96
ary1905 <127768541+ary1905@users.noreply.github.com>
2023-03-24 01:35:26 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+$host = "localhost"; 
+$user = "root"; 
+$password = ""; 
+$database = "university";
+$id = '';
+ 
+$con = mysqli_connect($host, $user, $password,$database);
+ 
+$method = $_SERVER['REQUEST_METHOD'];
+ 
+if (!$con) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+ 
+ 
+switch ($method) {
+    case 'GET':
+      $sql = "select * from instructor"; 
+      break;
+}
+ 
+// run SQL statement
+$result = mysqli_query($con,$sql);
+ 
+// die if SQL statement failed
+if (!$result) {
+  http_response_code(404);
+  die(mysqli_error($con));
+}
+ 
+if ($method == 'GET') {
+    if (!$id) echo '[';
+    for ($i=0 ; $i<mysqli_num_rows($result) ; $i++) {
+      echo ($i>0?',':'').json_encode(mysqli_fetch_object($result));
+    }
+    if (!$id) echo ']';
+  } elseif ($method == 'POST') {
+    echo json_encode($result);
+  } else {
+    echo mysqli_affected_rows($con);
+  }
+ 
+$con->close();
+?>
